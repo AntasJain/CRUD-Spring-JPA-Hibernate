b6bc20b950424148dfb4a85b44cc5991d7e79297
Antas Jain <jainantas.99@gmail.com>
2024-05-29 22:47:56 -0400

    Initial commit

---
+++ src/test/java/com/antasjain/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antasjain.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
